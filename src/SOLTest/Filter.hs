858a8fbe0f92cbb7bf5b322660f86c9caa98a847
Honza Dominik (250585) <xhonza04@vutbr.cz>
2026-04-14 22:53:26 +0200

    Filter.hs implemented all

--- src/SOLTest/Filter.hs
+++ src/SOLTest/Filter.hs
@@ -32,11 +32,24 @@ import SOLTest.Types
 -- The union of @selected@ and @filteredOut@ always equals the input list.
 --
 -- FLP: Implement this function using @matchesAny@ and @matchesCriterion@.
+-- Note for me: Filter all to include and to exclude then sub the exclude from include
+-- Workflow: load specs from FilterSpec arg, then check if to include all tests or check for criterium to keep
+-- then check for criterium to leftout, also save the ones leftout, then sub
 filterTests ::
   FilterSpec ->
   [TestCaseDefinition] ->
   ([TestCaseDefinition], [TestCaseDefinition])
-filterTests spec tests = undefined
+filterTests spec tests =
+  let useRegexSpec  = fsUseRegex spec -- Load specs for regex
+      includesSpecs = fsIncludes spec -- Load specs for include
+      excludesSpecs = fsExcludes spec -- Load specs for exclude
+      included =
+        if null includesSpecs -- If none includeSpec take all tests
+          then tests
+          else filter (matchesAny useRegexSpec includesSpecs) tests -- Apply criterions to select test to filter on
+      selected = filter (not . matchesAny useRegexSpec excludesSpecs) included -- Select final tests
+      filteredOut = filter (`notElem` selected) tests -- Save the left out
+   in (selected, filteredOut)
 
 -- | Check whether a test matches at least one criterion in the list.
 matchesAny :: Bool -> [FilterCriterion] -> TestCaseDefinition -> Bool
@@ -49,11 +62,18 @@ matchesAny useRegex criteria test =
 -- When @useRegex@ is 'True', the criterion value is treated as a POSIX
 -- regular expression matched against the relevant field(s).
 --
--- FLP: Implement this function. If you're not implementing the regex matching
--- bonus extension, you can either remove the first argument and update the usages,
--- or you can simply ignore the value.
+-- TODO REGEX Ignoring for now hence _ test criterion
+-- Note for me: By criterion type check for cond criteriums
 matchesCriterion :: Bool -> TestCaseDefinition -> FilterCriterion -> Bool
-matchesCriterion useRegex test criterion = undefined
+matchesCriterion _ test criterion =
+  case criterion of
+    ByAny raw -> 
+      let valTrimmed = trimFilterId raw
+        in tcdName test == valTrimmed
+        || tcdCategory test == valTrimmed
+        || valTrimmed `elem` tcdTags test
+    ByCategory raw -> tcdCategory test == trimFilterId raw -- Just compare category of test with criterium
+    ByTag raw -> trimFilterId raw `elem` tcdTags test
 
 -- | Trim leading and trailing whitespace from a filter identifier.
 trimFilterId :: String -> String
