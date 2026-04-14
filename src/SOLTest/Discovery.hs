cf43ebbfb05dabb4f9e1c297a26d4cd8b264f5e6
Honza Dominik (250585) <xhonza04@vutbr.cz>
2026-04-15 01:50:50 +0200

    Discover.hs Implmented

--- src/SOLTest/Discovery.hs
+++ src/SOLTest/Discovery.hs
@@ -14,15 +14,29 @@ import System.FilePath (replaceExtension, takeBaseName, (</>))
 -- Returns a list of 'TestCaseFile' records, one per @.test@ file found.
 -- The list is ordered by the file system traversal order (not sorted).
 --
--- FLP: Implement this function. The following functions may come in handy:
---      @doesDirectoryExist@, @takeExtension@, @forM@ or @mapM@,
---      @findCompanionFiles@ (below).
+-- Workflow: Foreach path run search, check if its dir if yes check fo tests if any capture them then look for
+-- more dirs and run nested search on them and gather their tests. This will give at end List of tests in this dir and
+-- multiple lists of tests for each subdir. At end concat so it returns just on huge list of test back to caller.
+-- This wf is for recursive, if not recursive skip part for searching in subdirs
 discoverTests :: Bool -> FilePath -> IO [TestCaseFile]
 discoverTests recursive dir = do
   entries <- listDirectory dir
   let fullPaths = map (dir </>) entries
-  -- ???
-  return [] -- replace [] with your list of discovered TestCaseFile
+  nested <- forM fullPaths $ \path -> do -- Foreach path run nested search
+    isDir <- doesDirectoryExist path -- Check if its dir
+    if isDir
+      then
+        if recursive
+          then discoverTests True path -- Check subdirs
+          else return []
+      else do -- Check if its file with correct extension
+        isFile <- doesFileExist path -- This is maybe overkill i guess
+        if isFile && takeExtension path == ".test"
+          then do
+            t <- findCompanionFiles path -- Look for in, out files so the test has what it needs
+            return [t]
+          else return []
+  return (concat nested) -- Concat so it return List of tests
 
 -- | Build a 'TestCaseFile' for a given @.test@ file path, checking for
 -- companion @.in@ and @.out@ files in the same directory.
